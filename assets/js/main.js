94bbe70c1a23100b5b83c3947f35a58940d271dd
Jesus Rojas <jarojas6524@misena.edu.co>
2022-04-21 05:32:36 -0500

    Project Success

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,26 @@
-async function obtenerDatos() {
-  const datos = new FormData()
-  datos.append('auth', 'loggin');
-  
-  const peticion = await fetch('./controller/readController.php', {
-    'Content-Type': 'application/json',
-    method: 'POST',
-    body: datos
-  })
-  const respuesta = await peticion.json();
-  // armar(respuesta)
-  mapa(armar(respuesta))
-}
+let accion = document.getElementById('accion')
+let fila = document.getElementById('fila')
+let puesto = document.getElementById('puesto')
 
 function mapa(res) {
   let html = 
-  `<div>
-    <div class="d-inline-block casilla border-none"></div>
-    <div class="d-inline-block casilla">1</div>
-    <div class="d-inline-block casilla">2</div>
-    <div class="d-inline-block casilla">3</div>
-    <div class="d-inline-block casilla">4</div>
-    <div class="d-inline-block casilla">5</div>
+  `<div class="d-flex justify-content-center">
+    <div class="d-inline-block text-white casilla"></div>
+    <div class="d-inline-block text-white casilla">1</div>
+    <div class="d-inline-block text-white casilla">2</div>
+    <div class="d-inline-block text-white casilla">3</div>
+    <div class="d-inline-block text-white casilla">4</div>
+    <div class="d-inline-block text-white casilla">5</div>
   </div>`;
-
-  for (const iterator of object) {
-    
-  }
-  
-
-  console.log(html)
-  document.getElementById('respuesta').innerHTML = html
+  res.forEach((iterator, index) => {
+    html += '<div class="d-flex justify-content-center">';
+    html += `<div class="d-inline-block text-white casilla">${index+1}</div>`;
+    for (const iterar of iterator) {
+      html += `<div class="d-inline-block text-white casilla">${iterar.text}</div>`;
+    }
+    html += '</div>';
+  });
+  document.getElementById('respuesta').innerHTML = html;
 }
 
 function armar(res) {
@@ -43,11 +34,77 @@ function armar(res) {
     const filtro = res.filter(data => data.row == i+1)
     arreglo.push([...filtro])
   }
+  for (const iterator of arreglo) {
+    iterator.sort((a,b) => {
+      if (a.col > b.col) {
+        return 1
+      }
+      if (a.col < b.col) {
+        return -1
+      }
+      return 0
+    })
+  }
   return arreglo;
 }
 
-function toggleButaca(id) {
-  
+function resetForm() {
+  accion.value = ''
+  fila.value = ''
+  puesto.value = ''
 }
 
-// obtenerDatos();
+async function obtenerDatos() {
+  const peticion = await fetch('./controller/readController.php')
+  const respuesta = await peticion.json();
+  mapa(armar(respuesta))
+}
+
+async function enviarDatos(){
+  if ([accion.value, fila.value, puesto.value].includes('')) {
+    new Toast({
+      message: 'Algunos campos se encuentran vacios verifica !!!',
+      type: 'danger'
+    });
+    return
+  }
+  if (!fila.value || fila.value < 0) {
+    new Toast({
+      message: 'La fila debe ser un numero valido',
+      type: 'danger'
+    });
+    return
+  }
+  if (!puesto.value || puesto.value < 0) {
+    new Toast({
+      message: 'El puesto debe ser un numero valido',
+      type: 'danger'
+    });
+    return
+  }
+  const formData = new FormData();
+  formData.append('accion', accion.value)
+  formData.append('fila', fila.value)
+  formData.append('columna', puesto.value)
+  const peticion = await fetch('./controller/transaccionController.php', {
+    method: 'POST',
+    'Content-Type': 'application/json',
+    body: formData
+  })
+  const { status, mensaje } = await peticion.json()
+  if (status) {
+    new Toast({
+      message: mensaje,
+      type: 'success'
+    });
+    resetForm();
+    obtenerDatos();
+  } else {
+    new Toast({
+      message: mensaje,
+      type: 'warning'
+    });
+  }
+}
+
+obtenerDatos();
--- index.php
+++ index.php
@@ -1,8 +1,3 @@
-<?php
-  // include_once('./utils/read.php');
-  // $sillas = consultar();
-  // echo json_encode('asdasd');
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,49 +5,35 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Desarrrollo Web PHP</title>
+  <link rel="stylesheet" href="./assets/css/toast.min.css">
   <link rel="stylesheet" href="./assets/css/styles.css">
 </head>
 <body>
   <div class="text-center">
     <h1 class="text-white pt-4">Teatro</h1>
-    <div class="text-center" id="respuesta">
-      
-      <table border="1" class="text-center">
-        <thead>
-          <tr>
-            <th colspan="6">Escenario</th>
-          </tr>
-          <tr>
-            <td></td>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            
-          
-          </tr>
-          <!-- <tr>
-            <td>1</td>
-            <td>V</td>
-            <td>V</td>
-            <td>V</td>
-            <td>V</td>
-            <td>V</td>
-          </tr> -->
-        </tbody>
-      </table>
+    <div class="text-center" id="respuesta"></div>
+    <div class="form">
+      <div class="pb-custom">
+        <input id="fila" type="number" placeholder="Fila">
+      </div>
+      <div class="pb-custom">
+        <input id="puesto" type="number" placeholder="Puesto">
+      </div>
+      <div class="pb-custom">
+        <select id="accion">
+          <option value="" selected disabled>Seleccione</option>
+          <option value="comprar">Comprar</option>
+          <option value="liberar">Liberar</option>
+          <option value="reservar">Reservar</option>
+        </select>
+      </div>
+      <div>
+        <button onclick="enviarDatos()">Enviar</button>
+        <button onclick="resetForm()">Borrar</button>
+      </div>
     </div>
-    <?php
-      // $sillas = consultar();
-      // echo json_encode($sillas);
-    ?>
   </div>
-  <!-- <div id="respuesta"></div> -->
-  <!-- <script src="./assets/js/main.js"></script> -->
+  <script src="./assets/js/toast.min.js"></script>
+  <script src="./assets/js/main.js"></script>
 </body>
 </html>
